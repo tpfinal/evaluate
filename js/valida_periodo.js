eba6434b811aa1c370f4bbc0a7e224b23fe33119
Diegarcho20 <diee_@live.com.ar>
2014-09-17 23:18:00 -0300

    avances en la interfaz y la validacion

---
+++ js/valida_periodo.js
@@ -0,0 +1,103 @@
+/************************** VALIDACION DEL FORMULARIO DE ALTA DE PERIODO ***********************************/
+(function($,W,D)
+{
+    var JQUERY4U = {};
+
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $('#crear_periodo').validate({
+                rules: {
+					nombre_periodo: {
+                        required: true
+                    },
+					cantidad: {
+                        required: true,
+						digits: true
+                    },
+				theDate1: {
+                        required: true
+                    },
+				theDate2: {
+                        required: true
+                    }
+                },
+				
+                messages: {
+					nombre_periodo: "* Ingrese un nombre para el periodo",
+                    
+					cantidad:{
+								required: "* Ingrese una cantidad de evaluaciones",				
+								digits: "* Ingrese solo numeros en la cantidad de evaluaciones"
+					}, 
+					
+					
+                    theDate1: "* Ingrese una fecha de inicio",				
+                    theDate2: "* Ingrese una fecha de finalizacion"				
+				},
+				
+				errorContainer: $("#divErrores"),
+				errorLabelContainer: "#divErrores ul",
+				errorElement: "span",
+				wrapper: "li",
+							
+                submitHandler: function(form) {
+					form.submit();
+                }
+				
+            });
+        }
+    }
+
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+
+})(jQuery, window, document);
+
+
+
+
+
+/************************** VALIDACION DEL FORMULARIO DE ALTA DE FECHAS DE EVALUACION ***********************************/
+(function($,W,D)
+{
+    var validador = {};
+
+    validador.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#agregar_fechas").validate({
+                rules: {
+				theDate1: {
+                        required: true
+                    },
+				theDate2: {
+                        required: true
+                    }
+                },
+				
+                messages: {				
+                    theDate1: "*",				
+                    theDate2: "*"				
+				},
+							
+                submitHandler: function(form) {
+					form.submit();
+                }
+				
+            });
+        }
+    }
+
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        validador.UTIL.setupFormValidation();
+    });
+
+})(jQuery, window, document);
